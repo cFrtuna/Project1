e4ab9ebb82bda4986ffb151c00b0ee783e81270c
Mihail Urmanschi <mihailurmanschi@Mihails-MacBook-Pro.local>
2021-06-24 22:49:28 -0700

    added main and sstaff email implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Email mail1 = new Email("misa", "urm");
+        mail1.setPassword("my password");
+        System.out.println(mail1.toString());
+
+        StaffEmail staffEmail = new StaffEmail("cristy","blabbla");
+        staffEmail.setPassword("crsty password");
+        System.out.println(staffEmail.toString());
+
+
+
     }
 }
---
+++ src/com/company/StaffEmail.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class StaffEmail extends Email{
+
+    public StaffEmail(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
+
+}
