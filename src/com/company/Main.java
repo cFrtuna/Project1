98b1f03ca9f3bde416c21610b06fb23fa2082dbe
cFrtuna <86384187+cFrtuna@users.noreply.github.com>
2021-07-03 22:29:38 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
-package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
+class Main {
+  public static void main(String[] args) {
+    CalculatorWithMemory cal = new CalculatorWithMemory();
+    cal.setMemory(34.6);
+    cal.recall();
+    System.out.println();
+    System.out.println("Accumulator after recalling: " + cal.getAccumulator());
+    cal.clearMemory();
+    cal.recall();
+    cal.save();
+    System.out.println("After Clearing Accumulator: " + cal.getAccumulator());
+  }
 }
